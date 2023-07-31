4c43ca2930201255f9bca73fa7cbc010e9af4616
eskres <einar.skreslett@gmail.com>
2023-07-31 14:45:04 +0100

    added settings modal to adjust pomodoro timings

--- src/PomodoroApp.js
+++ src/PomodoroApp.js
@@ -1,12 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import { Modal } from 'bootstrap';
 
 export default function PomodoroApp() {
   const [pause, setPause] = useState(true);
   const [task, setTask] = useState(true);
   const [taskTime, setTaskTime] = useState(25 * 60)
-  const [breakTime, setbreakTime] = useState(5 * 60)
+  const [breakTime, setBreakTime] = useState(5 * 60)
   const [remainingTime, setRemainingTime] = useState(taskTime)
-  
+
   useEffect(() => {
     const timer = setInterval(() => {
       if (!pause && remainingTime > 0) {
@@ -25,6 +26,57 @@ export default function PomodoroApp() {
     }, 1000);
     return () => {clearInterval(timer)}
   }, [pause, remainingTime, task, taskTime, breakTime]);
+  
+  function Settings() {
+    let modalRef = useRef();
+    
+    const [inputs, setInputs] = useState({
+      task: taskTime / 60,
+      break: breakTime / 60
+    })
+    
+    const handleChange = (e) => {
+      const name = e.target.name;
+      const value = e.target.value;
+      setInputs(values => ({...values, [name]: Number(value)}));
+    }
+
+    const handleSave = (e) => {
+      const modal = Modal.getOrCreateInstance(modalRef.current);
+      e.preventDefault();
+      setTaskTime(inputs.task * 60)
+      setBreakTime(inputs.break * 60)
+      setRemainingTime(inputs.task * 60)
+      modal.hide();
+    }
+
+    return (
+      <div className="modal" id="settings" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="modalTitle" aria-hidden="true" data-bs-theme="dark" ref={modalRef}>
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title text-light">Pomodoro Settings</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form className="modal-body" onSubmit={handleSave}>
+            <div className="form-floating mb-3 text-light">
+              <input type="number" className="form-control" id="taskTime" name="task" value={inputs.task} onChange={handleChange} min={5} max={1440} required></input>
+              <label htmlFor="taskTime">Task time (mins)</label>
+            </div>
+            <div className="form-floating text-light">
+              <input type="number" className="form-control" id="breakTime" name="break" value={inputs.break} onChange={handleChange} min={1} max={480} required></input>
+              <label htmlFor="breakTime">Break time (mins)</label>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-outline-light" data-bs-dismiss="modal">Close</button>
+              <button type="submit" className="btn btn-outline-light">Save changes</button>
+            </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   function Status() {
     if (pause) {
@@ -39,18 +91,19 @@ export default function PomodoroApp() {
 
   return (
     <>
-      <div className='row row-cols-auto gx-2'>
-        <h2 className='col text-white flex-grow-1 mb-0'>
-          Pomodoro Timer
-        </h2>
-        <div className='col d-flex align-items-center'>
-          <button className='btn btn-outline-light'>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
-            <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
-            <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
-          </svg>
-          </button>
-        </div>
+    <Settings/>
+    <div className='row row-cols-auto gx-2'>
+      <h2 className='col text-white flex-grow-1 mb-0'>
+        Pomodoro Timer
+      </h2>
+      <div className='col d-flex align-items-center'>
+        <button className='btn btn-outline-light' data-bs-toggle="modal" data-bs-target="#settings">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+          <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
+          <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
+        </svg>
+        </button>
+      </div>
       </div>
       <div className='container mb-4'>
         <div className="progress my-3" role="progressbar" aria-label="Timer progress bar" aria-valuenow={remainingTime} aria-valuemin="0" aria-valuemax={task ? taskTime : breakTime} style={{height: 3 + "px"}}>
@@ -66,14 +119,14 @@ export default function PomodoroApp() {
         {
           pause ?
           <>
-          <div className='col'>
-            <button type="button" className="btn btn-outline-light me-2 d-flex align-items-center" onClick={() => {setPause(false)}}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16">
-                <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"/>
-              </svg>
-              Start
-            </button>
-          </div>
+            <div className='col'>
+              <button type="button" className="btn btn-outline-light me-2 d-flex align-items-center" onClick={() => {setPause(false)}}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16">
+                  <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"/>
+                </svg>
+                Start
+              </button>
+            </div>
             <div className='col'>
               <button className="btn btn-outline-light me-2" onClick={() => {setRemainingTime(taskTime); setPause(true)}}>Reset</button>
             </div>
