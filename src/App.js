2d3a25a16376f9de44b99fb2c5ac578ba720d8cb
eskres <einar.skreslett@gmail.com>
2023-09-22 14:50:47 +0100

    Finished calculator app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ToDoApp from './ToDoApp'
 import PomodoroApp from './PomodoroApp'
+import CalculatorApp from './CalculatorApp'
 
 export default function App() {
   return (
     <div className='container'>
+      <CalculatorApp/>
       <PomodoroApp/>
       <ToDoApp/>
     </div>
