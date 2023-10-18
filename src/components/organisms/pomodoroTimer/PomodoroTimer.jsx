87b98a0bca4eec89ddf7d1f3f7d897950f08bd89
eskres <einar.skreslett@gmail.com>
2023-10-18 21:10:10 +0100

    refactored pomodoro timer into organisms

---
+++ src/components/organisms/pomodoroTimer/PomodoroTimer.jsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import Subtitle from "../../atoms/subtitle/Subtitle";
+import PomoConfigModal from "./pomoConfigModal/PomoConfigModal";
+import PomoStatusBadge from "./pomoStatusBadge/PomoStatusBadge";
+import PomoControlButtons from "./pomoControlButtons/PomoControlButtons";
+import PomoTimerCountdown from "./pomoTimerCountdown/PomoTimerCountdown";
+
+export default function PomodoroTimer() {
+  const [pause, setPause] = useState(true);
+  const [task, setTask] = useState(true);
+  const [config, setConfig] = useState(() => {
+      // Get any saved config from local storage
+      const saved = JSON.parse(localStorage.getItem("pomConfig"));
+      // Initialise state with saved config if they exist
+      if (saved) {
+        return saved
+      }
+      return {task: 25 * 60, break: 5 * 60}
+    }
+  );
+  const [remainingTime, setRemainingTime] = useState(config.task);
+
+  // Save user inputs to timer states
+  const handleSave = (inputs) => {
+    if (inputs.task > 0 && inputs.break > 0) {      
+      setTask(true);
+      setConfig({task: inputs.task * 60, break: inputs.break * 60});
+      setRemainingTime(inputs.task * 60);
+    }
+  }
+
+  useEffect(() => {
+    // Store to-dos in local storage on state change
+    localStorage.setItem("pomoConfig", JSON.stringify(config))
+  }, [config])
+
+  // For timer and transitioning between task and break
+  useEffect(() => {
+    const timer = setInterval(() => {
+      // Timer
+      if (!pause && remainingTime > 0) {
+        setRemainingTime((remainingTime) => remainingTime -1);
+      }
+      //Transition to break
+      if (remainingTime === 0 && task) {
+        setRemainingTime(config.break);
+        setTask(prevState => !prevState);
+      }
+      //Transition to task
+      if (remainingTime === 0 && !task) {
+        setRemainingTime(config.task);
+        setTask(prevState => !prevState);
+      }
+    }, 1000);
+    return () => {clearInterval(timer)}
+  }, [pause, remainingTime, task, config]);
+
+  return (
+  <>
+    <PomoConfigModal
+      config={config}
+      handleSave={handleSave}
+    />
+    <Subtitle className="col">
+      Pomodoro Timer
+      <PomoStatusBadge
+        pause={pause}
+        task={task}
+        config={config}
+        remainingTime={remainingTime}
+      />
+    </Subtitle>
+    <div className="container mb-4">
+      <PomoTimerCountdown
+        remainingTime={remainingTime}
+        task={task}
+        config={config}
+        pause={pause}
+      />
+      <PomoControlButtons
+        pause={pause}
+        setPause={setPause}
+        setRemainingTime={setRemainingTime}
+        config={config}
+      />
+    </div>
+  </>
+  )
+}
---
+++ src/components/organisms/pomodoroTimer/pomoControlButtons/PomoControlButtons.jsx
@@ -0,0 +1,30 @@
+import Button from "../../../atoms/button/Button";
+
+export default function PomoControlButtons({ pause, setPause, setRemainingTime, config }) {
+  return (
+    <div className="row row-cols-auto">
+      {
+        // Conditionally render start and reset or pause buttons
+        pause ?
+          <div className="col d-flex flex-grow-1">
+            <Button className={"d-flex align-items-center me-2"} onClick={() => setPause(false)} icon="play">
+              Start
+            </Button>
+            <Button onClick={() => {setRemainingTime(config.task); setPause(true);}}>
+              Reset
+            </Button>
+          </div>
+        :
+        <div className="col flex-grow-1">
+          <Button className={"d-flex align-items-center me-2"} onClick={() => setPause(true)} icon={"pause"}>
+            Pause
+          </Button>
+        </div>
+      }
+      <div className="col">
+        <Button className={"d-flex align-items-center h-100"} dataBsToggle={"modal"} dataBsTarget={"#configModal"} onClick={() => {setPause(true)}} icon={"gear"}>
+        </Button>
+      </div>
+    </div>
+  )
+}
