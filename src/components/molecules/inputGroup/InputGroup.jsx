20e2f98d5bff5f5f7466f52d761285e6bfd5ac90
eskres <einar.skreslett@gmail.com>
2023-10-22 22:25:38 +0100

    removed explicit return

--- src/components/molecules/inputGroup/InputGroup.jsx
+++ src/components/molecules/inputGroup/InputGroup.jsx
@@ -4,31 +4,29 @@ import Paragraph from "../../atoms/paragraph/Paragraph";
 
 export default function InputGroup({ fields, className }) {
   return (
-    fields.map((value, index) => {
-      return (
-        <div className={className} key={index}>
-          <Input
-            type={value.type}
-            inputMode={value.inputMode}
-            pattern={value.pattern}
-            id={value.id}
-            name={value.name}
-            value={value.value}
-            onChange={value.onChange}
-            onBlur={value.onBlur}
-            min={value.min}
-            max={value.max}
-            required={value.required}
-          />
-          <Label className={value.label.className} htmlFor={value.label.htmlFor}>
-            {value.label.content}
-          </Label>
-          <Paragraph className={value.feedback.className}>
-            {value.feedback.value}
-          </Paragraph>
-        </div>
-      )
-    })
+    fields.map((value, index) => 
+      <div className={className} key={index}>
+        <Input
+          type={value.type}
+          inputMode={value.inputMode}
+          pattern={value.pattern}
+          id={value.id}
+          name={value.name}
+          value={value.value}
+          onChange={value.onChange}
+          onBlur={value.onBlur}
+          min={value.min}
+          max={value.max}
+          required={value.required}
+        />
+        <Label className={value.label.className} htmlFor={value.label.htmlFor}>
+          {value.label.content}
+        </Label>
+        <Paragraph className={value.feedback.className}>
+          {value.feedback.value}
+        </Paragraph>
+      </div>
+    )
   )
 }
 
