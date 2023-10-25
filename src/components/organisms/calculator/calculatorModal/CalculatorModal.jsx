323716dd4b1adaf2b64f8d86628c5ad0fe31e321
eskres <einar.skreslett@gmail.com>
2023-10-25 22:19:38 +0100

    created calculator title atom for calculator modal

--- src/components/organisms/calculator/calculatorModal/CalculatorModal.jsx
+++ src/components/organisms/calculator/calculatorModal/CalculatorModal.jsx
@@ -1,14 +1,14 @@
 import Modal from '../../../molecules/modal/Modal';
-import CalculatorIcon from '../../../atoms/Icons/SVGs/CalculatorIcon';
 import CalculatorDisplay from '../calculatorDisplay/CalculatorDisplay';
 import CalculatorKeypad from '../calculatorKeypad/CalculatorKeypad';
+import CalculatorTitle from '../../../molecules/calculatorTitle/CalculatorTitle';
 
 export default function CalculatorModal(props) {
   return (
     <>
       <Modal
         id={"calculatorModal"}
-        title={<CalculatorIcon/>}
+        title={<CalculatorTitle>Calculator</CalculatorTitle>}
         body={
           <>
             <div className="row mb-2 py-2">
