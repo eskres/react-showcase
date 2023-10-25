0fa32865b2fdbf5c8dd25a37b1cce71447256d52
eskres <einar.skreslett@gmail.com>
2023-10-25 20:17:29 +0100

    deleted title and subtitle components

--- src/components/atoms/subtitle/Subtitle.jsx
+++
@@ -1,5 +0,0 @@
-export default function Subtitle({ className, children }) {
-    return (
-        <h2 className={className ? `text-white ${className}` : "text-white"}>{children}</h2>
-    )
-}
--- src/components/atoms/title/Title.jsx
+++
@@ -1,5 +0,0 @@
-export default function Title({ className, children }) {
-    return (
-        <h1 className={className ? `text-white ${className}` : "text-white"}>{children}</h1>
-    )
-}
