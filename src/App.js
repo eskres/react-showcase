af5f56ee3cbab78c629417aed26266f5cf8ab55f
eskres <einar.skreslett@gmail.com>
2023-10-25 22:17:43 +0100

    removed old header and footer sections

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import ToDoList from './components/organisms/toDoList/ToDoList';
 import PomodoroTimer from './components/organisms/pomodoroTimer/PomodoroTimer';
 import Calculator from './components/organisms/calculator/Calculator';
+import Header from './components/molecules/header/Header';
+import Footer from './components/molecules/footer/Footer';
 
 export default function App() {
   return (
-    <div className='container'>
-      <Calculator/>
-      <PomodoroTimer/>
-      <ToDoList/>
-    </div>
+    <>
+      <Header/>
+      <div className='container'>
+        <Calculator/>
+        <PomodoroTimer/>
+        <ToDoList/>
+      </div>
+      <Footer/>
+    </>
   )
 }
