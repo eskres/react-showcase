c29c176624a47df70583042b22e1c4795c2620b6
eskres <einar.skreslett@gmail.com>
2023-10-25 22:13:59 +0100

    updated icon component with new icons

--- src/components/atoms/Icons/Icon.jsx
+++ src/components/atoms/Icons/Icon.jsx
@@ -1,6 +1,11 @@
 import Gear from './SVGs/Gear';
 import Play from './SVGs/Play';
 import Pause from './SVGs/Pause';
+import CalculatorIcon from './SVGs/CalculatorIcon';
+import React from './SVGs/React';
+import Github from './SVGs/Github';
+import LinkedIn from './SVGs/LinkedIn';
+import Email from './SVGs/Email';
 
 export default function Icon({ icon }) {
   switch (icon) {
@@ -12,6 +17,21 @@ export default function Icon({ icon }) {
 
     case "pause":
       return <Pause/>
+
+    case "calculator":
+      return <CalculatorIcon/>
+      
+    case "react":
+      return <React/>
+      
+    case "github":
+      return <Github/>
+      
+    case "linkedin":
+      return <LinkedIn/>
+      
+    case "email":
+      return <Email/>
 
     default:
       return null;
