257c4f351455d43bb384b26c6f1203e0e9de5070
eskres <einar.skreslett@gmail.com>
2023-10-28 14:46:28 +0100

    continuing to refactor atoms into typescript

--- src/components/atoms/Icons/SVGs/Gear.tsx
+++ src/components/atoms/Icons/SVGs/Gear.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import { SVGProps } from "svgInterfaces";
 
-export default function Gear({ className }) {
+export default function Gear({ className }:SVGProps ): React.JSX.Element {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={className ? `bi bi-gear ${className}` : "bi bi-gear mx-1"} viewBox="0 0 16 16">
         <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
