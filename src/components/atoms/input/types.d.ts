896a6fc86157ef021923c6bfb0d8688404462a14
eskres <einar.skreslett@gmail.com>
2023-10-30 23:00:46 +0000

    corrected onBlur event type for inputs

--- src/components/atoms/input/types.d.ts
+++ src/components/atoms/input/types.d.ts
@@ -1,5 +1,5 @@
 declare module "inputInterfaces" {
   export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    onBlur?: (e: React.FormEvent<HTMLInputElement>) => void;
+    onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
   }
 }
