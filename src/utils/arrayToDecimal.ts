6faf10b4963abb847a4a93fb269e758e4b1a06cd
eskres <einar.skreslett@gmail.com>
2023-10-30 23:01:32 +0000

    refactored util functions to typescript

--- src/utils/arrayToDecimal.ts
+++ src/utils/arrayToDecimal.ts
@@ -1,7 +1,7 @@
 import Decimal from 'decimal.js';
 
 // Convert num arrays to Decimal instances for calculations
-export default function arrayToDecimal(num) {
+export default function arrayToDecimal(num: []): Decimal | [] {
 	if (num.length !== 0 && !(num instanceof Decimal)) {
 		return new Decimal(num.join(""));
 	}
