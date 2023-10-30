701390479c69d41ac49a4eef5abec9e32b774327
eskres <einar.skreslett@gmail.com>
2023-10-30 22:26:43 +0000

    fixed issues with types for link component

--- src/components/atoms/link/types.d.ts
+++ src/components/atoms/link/types.d.ts
@@ -1,6 +1,7 @@
 declare module "linkInterfaces" {
   export interface LinkProps extends React.AnchorHTMLAttributes<HTMLElement> {
-    children?: string;
+    children?: string | React.JSX.Element;
     ariaLabel?: string;
+    style?: React.CSSProperties
   }
 }
